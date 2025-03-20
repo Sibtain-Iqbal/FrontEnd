80505c3815c8e4b205ad24a4a70c907570eaf92f
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-03-20 03:51:34 -0700

    control_structure and objects

---
+++ Control_Structures/Objects.js
@@ -0,0 +1,37 @@
+let person = {
+
+    name: "Sibtain",
+    Age: 19,
+    id: 0
+}
+
+console.log(`"my name is ${person.name} and age is ${person.Age} id is ${person.id}"`);
+
+
+
+let student = {
+
+    name: "Sibtain",
+    Age: 19,
+    id: 0,
+    personal: {
+        contact: "0355",
+        uniersity: "karkrom internationl university",
+        location: "Danyoure"
+
+    }
+
+
+}
+student.experience = 2,
+console.log(student);
+
+
+inform = `" Student name : ${person.name}
+        Student age : ${person.Age} 
+        Student id  ${person.id} 
+        Student contact  ${student.personal.contact}
+         university ${student.personal.uniersity}
+        Student location ${student.personal.location}
+        "`
+console.log(inform);
--- Control_Structures/condition.js
+++ Control_Structures/condition.js
@@ -1,1 +1,75 @@
-console.log("heloo world");
+let marrks = 100;
+switch(marrks) {
+    case 100 :
+        console.log("A+");
+        break;
+    case  90 :
+        console.log("A-");
+    default :
+    console.log("Fail");
+    
+            
+}
+
+let days  = [1,2,3,4,5,6];
+
+
+switch (days) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Thruday");
+        break;
+    case 4:
+        console.log("friday");      
+        break;
+    case 5:
+        console.log("saturday");
+        break;
+    case 6:
+        console.log("sunday");
+        break;
+    
+    default:
+        console.log("Weekend");
+        
+        break;
+}
+
+
+
+
+let  nums1 = 2;
+let nums2 = 5;
+
+let operands = "minimize";
+
+switch(operands){
+
+    case "Addition":
+    console.log( nums1 +  nums2);
+    
+    break;
+
+    case "Multiply":
+        mul = nums1 *  nums2
+        console.log(mul);
+        break;
+
+    case "divide":
+            divide = nums1 /  nums2
+            console.log(divide);
+            break;
+            
+    case "minimize":
+                mini = nums1 - nums2
+                console.log(mini);
+                break;
+    default :
+    console.log("nothing");
+    
+}
