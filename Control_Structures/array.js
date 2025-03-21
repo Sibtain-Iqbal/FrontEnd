509cae0dca3ac66eb21796f87da5aaa2998158d7
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-03-21 03:51:47 -0700

    methodds oif arry

---
+++ Control_Structures/array.js
@@ -0,0 +1,60 @@
+let arr = [1, 2, 3];
+ arr.push(4, 5); 
+console.log(arr); 
+// //  add the index in the elemnt
+
+
+
+let arr22 = [1, 2, 3];
+arr.pop();
+console.log(arr22); 
+
+// // remove the last elemnt
+
+let newarrzz = [1,2,3,4,5,6]
+newarr.Append()
+console.log(newarr);
+
+
+let arrr = [2, 3];
+arr.unshift(0, 1);
+console.log(arrr); 
+
+// // let Arrays = [1,'a',2]
+
+// // console.log(Arrays[1]);
+
+
+// let arrshi = [0, 1, 2,3,4,5,6,7];
+
+// console.log(arr.shift()); // Output: [1, 2]
+
+
+let arrs22 = ['Hello', 'World',1];
+console.log(arr22.join(' ')); // Output: "Hello World"
+
+
+// 
+let arr1 = [1, 2];
+// let arr2 = [1, 2];
+// let result = arr1.concat(arr2);
+// console.log(result); // Output: [1, 2, 3, 4]
+
+
+
+let arrrr = [1, 2, 3, 4, 5];
+let sliced = arr.slice(1, 4); // index 1 to 3
+console.log(sliced); // Output: [2, 3, 4]
+// Add elements at index 1
+arr.splice(1, 0 , 99, 88);
+console.log(arr); // [10, 99, 88, 40, 50]
+
+
+
+
+let newarr = [1,2,3,4,5,6,7]
+
+console.log(newarr.splice(2,5,"banana","grpes","kiwi"));
+
+
+
