69383a961afcf2c1f1bbe3f7f1f08b14b153553d
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-03-26 03:14:45 -0700

    loops

---
+++ Loops/loops.js
@@ -0,0 +1,26 @@
+// for (i=1; i <=10; i++ ){
+//     console.log("for lop", i);
+    
+// }
+
+
+// let i =1 ;
+// while(i => 10){
+//     i++
+//     console.log(i);
+    
+// }
+
+for (a = 0; a<=10 ; a++){
+    // console.log(`2 x ${a} = ${2 * a}`);
+    
+}
+
+let arr = ['A','B','C','D','E']
+
+for (let i = arr.length; i >= 0; i++) {
+
+    console.log(i);
+    
+    
+}
