9b88186f48c028c53ba8df695f4e969974731bd3
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-03-27 01:58:20 -0700

    practice work

---
+++ Loops/wrok.js
@@ -0,0 +1,26 @@
+function sumevennumbers(n) {
+    let sum = 0;
+    for (let i = 2; i <= n; i += 2) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sumevennumbers(10));
+
+
+
+
+// prpgram 2
+
+
+function countDigits(num) {
+    let count = 0;
+    do {
+        count++;
+        num = Math.floor(num/10);
+    } while (num > 0);
+    return count;
+}
+
+console.log(countDigits(10)); 
