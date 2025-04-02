e34fe2bda8e419ebc638dc30ab022f8ed0361c4f
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-04-02 11:19:40 -0700

    Create func.js

---
+++ Quiz/func.js
@@ -0,0 +1,78 @@
+
+function canVote(age) {
+    if (age >= 18 ){
+        return age >= 18;
+    }
+    
+
+}
+
+function VotingEliglibility(age) {
+    if (canVote(age)) {
+      return "you can Vote !!";
+    } 
+
+    else {
+      return `Wait ${ 18 - age} more years to vote.`;
+    }
+  }
+
+  
+console.log(VotingEliglibility(10))
+
+
+
+
+
+
+
+
+//drving eligibility
+
+
+
+function candrive(age){
+    if (age>=16){
+        return age 
+    }
+}
+
+
+function getlicence(age){
+    if (candrive(age)){
+        console.log("you are eligible to drive car");
+        
+    }
+    else{
+        return (`you are not eligilbe to drive car wait ${19 -age} year !`);
+        
+    }
+}
+
+
+console.log(getlicence(10));
+
+
+
+//  buying 
+
+
+function canbuy(balance,price){
+    if (balance>= price){
+        return balance,price
+    }
+}
+
+
+function buying(balance,price){
+    if (canVote(balance,price)){
+        return `your account balcance iss ${balance} buy something `
+    }
+    else{
+        return `your account is balance is less than for buy on this ${price}PKR`
+    }
+
+}
+
+console.log(buying(1000,400));
+
