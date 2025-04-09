f5fcd22b6539e2c6725668241fa91d3f61b35dfc
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-04-09 05:39:42 -0700

    get date

--- dom/events.js
+++ dom/events.js
@@ -16,3 +16,30 @@ const inputs = document.getElementById('input')
 inputs.addEventListener("keypress",(e)=>{
     para.textContent = e.target.value
 })
+
+
+
+
+
+// set time oyt and setintervals
+
+setInterval(timeCalcualtor,1000)
+
+
+
+function timeCalcualtor (){
+const date = new Date()
+const second = date.getSeconds()
+const minute = date.getMinutes()
+const hours = date.getHours()
+document.body.innerHTML = ''
+document.writeln(hours + ":",minute + ":",second)
+
+
+}
+
+
+
+
+
+
