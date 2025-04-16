5ac0276b225f76d3c439897baa7e35b25a9dd1c5
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-04-16 05:52:34 -0700

    tofolist

---
+++ todolist/todo.js
@@ -0,0 +1,10 @@
+
+const input = document.getElementById("input")
+const button =  document.getElementById("btn")
+ 
+
+button.addEventListener('click',()=>{
+   console.log(e.target.value);
+   
+
+})
