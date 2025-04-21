3345ebe4c18224d4f8a088ab65a9450be5e4de89
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2025-04-21 08:26:37 -0700

    Integrate Github APis

---
+++ Promises/app.js
@@ -0,0 +1,55 @@
+// console.log("besfore settime out");
+
+
+
+// promise acceppt
+
+// const asycronsesHandle = ()=>{
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve("succelfully resolve")
+            
+//         },[5000])
+//     })
+// }
+// console.log("besfore settime out");
+// asycronsesHandle().then((data)=>{
+//     console.log(data);
+    
+// })
+
+
+
+const username = document.getElementById("username")
+const img = document.getElementById("img")
+const section = document.getElementById("section")
+
+
+const fetchgithubuser = ()=>{
+    fetch("https://api.github.com/user/Sibain-Iqbal")
+    .then((data)=>{
+        return data.json();
+    })
+    .then((parsedData)=>{
+        console.log("fetch");
+        parsedData.forEach((users)=>{
+           const div =  document.createElement("div")
+           const image = document.createElement("img")
+         const heading =  document.createElement("h1")
+         image.src = users.avatar_url;
+            heading.textContent = `USER NAME : ${users.login} `
+            div.appendChild(image)
+            div.appendChild(heading)
+            section.appendChild(div)
+            
+
+
+        });
+        
+    })
+    .catch((error)=>{
+        console.log("ERROR FOUND",error);
+        
+    })
+}
+fetchgithubuser()
